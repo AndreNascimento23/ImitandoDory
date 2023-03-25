261024c287a56923b3ec434aa61cbe5c4564bd38
André Nascimento <andre4.20nascimento@outlook.com>
2023-03-25 18:38:21 -0300

    Comcluido

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿Console.Clear();
+
+Console.WriteLine(" --- Imitando Dory ---\n");
+string frase = Console.ReadLine()!;
+string dory = frase
+
+        .Replace("A","AAA" )
+        .Replace("a", "aaa")
+        .Replace("E","EEE")
+        .Replace("e","eee")
+        .Replace("I","III")
+        .Replace("i","iii")
+        .Replace("O","OOO")
+        .Replace("o","ooo")
+        .Replace("U","UUU")
+        .Replace("u","uuu");
+
+Console.Clear();
+        Console.ForegroundColor = ConsoleColor.DarkRed;
+Console.Write("----- Falando em Baleies -----\n\n");
+
+        Console.ForegroundColor = ConsoleColor.DarkBlue;
+
+Console.WriteLine($"{dory}\n\n");
+        Console.ResetColor();
+
+       
